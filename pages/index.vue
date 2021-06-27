3f4b28812afc450ab5450899d8950e227c95faf0
coffeekitkat <coffeekitkat39@gmail.com>
2021-06-27 16:01:12 +0000

    chore: format code using prettier

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,24 @@
 <template>
-<div class="bg-gray-800">
+  <div class="bg-gray-800">
     <div class="flex p-16 items-center justify-center">
-    <div class="items-center justify-center flex grid lg:grid-cols-3 md:grid-cols-2 gap-4">
-     <div v-for="p in panelsDb" :key="p.filename" class="text-center mb-2">
-       <img :src="p.path" :alt="p.filename" >
-       <code class="text-blue-400">{{ p.filename }}</code>
-     </div>
-     </div> 
-    </div></div>
+      <div
+        class="
+          items-center
+          justify-center
+          flex
+          grid
+          lg:grid-cols-3
+          md:grid-cols-2
+          gap-4
+        "
+      >
+        <div v-for="p in panelsDb" :key="p.filename" class="text-center mb-2">
+          <img :src="p.path" :alt="p.filename" />
+          <code class="text-blue-400">{{ p.filename }}</code>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -16,9 +27,9 @@ import panelsDb from '~/static/panellist.json'
 export default {
   data() {
     return {
-      panelsDb
+      panelsDb,
     }
-  }
+  },
 }
 </script>
 
