a2c8a6ebd74226bd2ba74425942bc390cf793194
coffeekitkat <82024111+coffeekitkat@users.noreply.github.com>
2024-11-11 19:22:22 +0800

    feat: update panels

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,7 +14,8 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'ak-panels', name: 'Aura Kingdom Panels', content: 'Aura Kingdom Panels' }
+      { hid: 'ak-panels', name: 'Aura Kingdom Panels', content: 'Aura Kingdom Panels' },
+      { hid: 'description', name: 'description', content: 'Aura Kingdom Panels' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
